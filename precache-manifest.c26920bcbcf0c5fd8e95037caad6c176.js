65994a4d1741cc352695b1283cddd66f0a587bda
Gourav <gouravapgis@gmail.com>
2021-05-25 21:59:11 +0530

    Updates

---
+++ precache-manifest.c26920bcbcf0c5fd8e95037caad6c176.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7cdbc6cd72e4c86e8da66efa28a381",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "33fccc0e37b2dd6e9126",
+    "url": "/monster-rolodex/static/css/main.f0b15f5a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33fccc0e37b2dd6e9126",
+    "url": "/monster-rolodex/static/js/main.0ecc3c51.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
